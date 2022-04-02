3da071f3943e8c876acb19470f37a87beec94012
Augusto Varela <gustivarela@hotmail.com>
2022-04-02 16:37:17 -0300

    Added test card and made cards load data from their scriptable object

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,10 +8,18 @@ public class Card : MonoBehaviour {
 
     public Button cardButton;
 
+    public TMP_Text cardTitle;
+    public TMP_Text cardDescription;
+    public Image cardImage;
+
     public CardData data;
     // Start is called before the first frame update
     void Start() {
-
+        if (data) {
+            cardTitle.text = data.cardName;
+            cardDescription.text = data.cardDescription;
+            cardImage.sprite = data.cardIcon;
+        }
     }
 
     // Update is called once per frame
