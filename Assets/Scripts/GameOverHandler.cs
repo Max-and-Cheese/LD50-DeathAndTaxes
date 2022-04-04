5c69617580d7c8fef7ae72800cb5ede54eca1de2
Augusto Varela <gustivarela@hotmail.com>
2022-04-03 22:01:00 -0300

    Added gameover data and fixed screen sizing

---
+++ Assets/Scripts/GameOverHandler.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameOverHandler : MonoBehaviour
+{
+
+
+    public TMP_Text causeText;
+    public TMP_Text dayMoneyText;
+
+    private int days;
+    private int money;
+    private string cause;
+    // Start is called before the first frame update
+    void Start()
+    {
+        days = PlayerPrefs.GetInt("days");
+        money = PlayerPrefs.GetInt("money");
+        cause = PlayerPrefs.GetString("cause");
+
+        dayMoneyText.text = $"You lasted {days} days with ${money} in evaded taxes!";
+        causeText.text = cause;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
